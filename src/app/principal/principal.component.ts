62188a3dc7c7e97fe4b82483e5eade9323c7deb2
fquiroz <fernando.equiroz@gmail.com>
2018-04-06 02:23:55 -0300

    oka

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -17,7 +17,32 @@ export class PrincipalComponent implements OnInit {
 
   private _mobileQueryListener: () => void;
 
-  @ViewChild('sidenav') sidenavComponent: SidenavComponent;
+ @ViewChild('sidenav') sidenavComponent: SidenavComponent;
+
+
+
+
+  fillerContent = Array(10).fill(0).map(() =>
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
+
+
+
+ 
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
+
+
+
+
+
   constructor(private authService:AuthService,private route: ActivatedRoute,private router:Router,changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
@@ -27,28 +52,22 @@ export class PrincipalComponent implements OnInit {
   ngOnInit() {
   }
 
+
+  
+  
+
+
+
+
+
   logout(){
-  this.authService.logout().subscribe(data=>{
-    console.log('logout OK');
-    this.router.navigate(['/login']);
-  },error=>{
-    console.log('logout Error',error);
-  });
-  
-  }
-
-  
-  fillerNav = Array(5).fill(0).map((_, i) => `Nav Item ${i + 1}`);
-
-  fillerContent = Array(3).fill(0).map(() =>
-      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`);
-
-  ngOnDestroy(): void {
-    this.mobileQuery.removeListener(this._mobileQueryListener);
-  }
+    this.authService.logout().subscribe(data=>{
+      console.log('logout OK');
+      this.router.navigate(['/login']);
+    },error=>{
+      console.log('logout Error',error);
+    });
+    
+    }
 
 }
